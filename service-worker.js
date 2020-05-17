0d3ebd6ab69de1203e5b78b95729fbf1a7ebb90e
k <k@aura.top>
2020-05-17 18:35:57 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/zen-positive/precache-manifest.fdc0b48d28620e71fb5398901238cb92.js"
+  "/zen-positive/precache-manifest.584e35fe0811add8f9b6556c7153a94b.js"
 );
 
 self.addEventListener('message', (event) => {
