f6f03017c0b0314adb12683af8cc7ae4a49dcc02
k <k@aura.top>
2020-05-17 18:11:49 +0300

    Updates

---
+++ precache-manifest.0c9df384a12b5fb194c55042b10096e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e545f89ac1accc0c2ce22b4c8bf87015",
+    "url": "/zen-positive/index.html"
+  },
+  {
+    "revision": "5153b83a96e6410c284f",
+    "url": "/zen-positive/static/css/main.241da543.chunk.css"
+  },
+  {
+    "revision": "d1fbf38753308e52c673",
+    "url": "/zen-positive/static/js/2.74249cde.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/zen-positive/static/js/2.74249cde.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5153b83a96e6410c284f",
+    "url": "/zen-positive/static/js/main.44ec2429.chunk.js"
+  },
+  {
+    "revision": "52f06ac0eee645501de2",
+    "url": "/zen-positive/static/js/runtime-main.107a1ded.js"
+  }
+]);
