d6653c3a8911686937fde855494fec2a3fa65889
k <k@aura.top>
2020-05-17 19:18:17 +0300

    wip

--- src/components/Main/Controls/index.js
+++ src/components/Main/Controls/index.js
@@ -1,24 +1,24 @@
-import React, { useCallback, useEffect, useContext } from 'react';
+import React, { useEffect } from 'react';
 import { IS_FULLSCREEN_AVAILABLE } from '../../../utils';
 import cls from "classnames";
 import './index.css';
 
-export const Controls = ({ className, onReload, isReloading }) => {
-  const toggleFullscreen = useCallback(() => {
-    if (!document.fullscreenElement) {
-      document.documentElement.requestFullscreen();
-    } else {
-      document.exitFullscreen();
-    }
-  }, []);
+const toggleFullscreen = () => {
+  if (!document.fullscreenElement) {
+    document.documentElement.requestFullscreen();
+  } else {
+    document.exitFullscreen();
+  }
+};
 
+export const Controls = ({ className, onReload, isReloading }) => {
   useEffect(() => {
     const handleKeyDown = ({ code } = {}) => code === 'KeyF' && toggleFullscreen();
     document.addEventListener('keydown', handleKeyDown, { passive: true });
     return () => {
       document.removeEventListener('keydown', handleKeyDown, { passive: true });
     }
-  }, [toggleFullscreen]);
+  }, []);
 
   return <div className={cls('controls', className)}>
     { IS_FULLSCREEN_AVAILABLE && <button onClick={toggleFullscreen}><span>f</span>ullscreen</button> }
