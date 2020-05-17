40604e4eea2ccc0792393d09085fd86aab664e16
k <k@aura.top>
2020-05-17 18:41:35 +0300

    wip

--- src/components/Main/Top/index.js
+++ src/components/Main/Top/index.js
@@ -1,3 +1,5 @@
 import React from "react";
+import cls from 'classnames';
+import './index.css';
 
-export const Top = ({ className }) => (<div className={className}>Zen Positive</div>);
+export const Top = ({ className }) => (<div className={cls('top', className)}>Zen Positive</div>);
